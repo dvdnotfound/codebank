436cfff7a9647b3a3ea626a4149fc11b55bba304
dvdnotfound <contato.dvdsantos@gmail.com>
2022-05-25 07:07:19 -0300

    Abstração de atributo

--- src/codebank/Conta.java
+++ src/codebank/Conta.java
@@ -2,7 +2,7 @@ package codebank;
 
 public class Conta {
 
-	double saldo;
+	private double saldo;
 	int agencia;
 	int numero;
 	Cliente titular;
@@ -29,5 +29,9 @@ public class Conta {
 			return true;
 		}
 		return false;
+	}
+	
+	public double consulta() {
+		return this.saldo;
 	}
 }
