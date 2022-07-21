d3e96006cc1e866a7b1a9834adf57720abc28331
dvdnotfound <contato.dvdsantos@gmail.com>
2022-07-21 05:32:26 -0300

    Atributo estático

--- src/codebank/Conta.java
+++ src/codebank/Conta.java
@@ -3,9 +3,18 @@ package codebank;
 public class Conta {
 
 	private double saldo;
-	int agencia;
-	int numero;
-	Cliente titular;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total;
+	
+	public Conta(int agencia, int numero) {
+		Conta.total++;
+		System.out.println("O total de contas é: " + total);
+		this.agencia = agencia;
+		this.numero = numero;
+		System.out.println("estou criando uma conta " + this.numero);
+	}
 
 	void deposita(double valor) {
 		this.saldo += valor;
@@ -34,4 +43,45 @@ public class Conta {
 	public double consulta() {
 		return this.saldo;
 	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public Cliente getTitular() {
+		return titular;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+	public static int getTotal() {
+		return total;
+	}
+
+	public static void setTotal(int total) {
+		Conta.total = total;
+	}
+	
 }
